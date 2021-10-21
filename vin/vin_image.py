ba53d59b2b890f235efe68594cdf8099e6cc7508
Asmilex <andresmm@outlook.com>
2021-10-21 10:00:24 +0200

    Crea estructura de imagen
    Avanza #9

---
+++ vin/vin_image.py
@@ -0,0 +1,13 @@
+class VinImage:
+    def __init__(self, file_path: str):
+        self.file        # Usar Image de PIL.
+        self.tag = 'unknown'    # Correspondiente a la carpeta en la que se encuentra.
+
+    def change_tag(self, nuevo_tag: str):
+        pass
+
+    def resolution(self):
+        pass
+
+    def downscale(resolucion: tuple[int, int]):
+        pass
