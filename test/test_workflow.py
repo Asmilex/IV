6d0476c819df80f59fc4d81f7c2d6ae1ca28746b
Asmilex <andresmm@outlook.com>
2021-10-26 19:02:21 +0200

    Cambio de nombre a las funciones relacionadas con los tests de las imágenes #15

--- test/test_workflow.py
+++ test/test_workflow.py
@@ -5,13 +5,13 @@ def test_image_creation():
     imagen = VinImage('./test/img/test_image.jpg')
     assert imagen != None
 
-def test_downscale():
+def test_image_downscale():
     resolucion: tuple[int, int] = (64, 64)
     imagen = VinImage('./test/img/test_image.jpg')
     imagen.downscale(resolucion)
     assert imagen.resolution() == resolucion
 
-def test_tag():
+def test_image_tag():
     imagen = VinImage('./test/img/test_image.jpg')
     nuevo_tag = 'prueba!'
     imagen.change_tag(nuevo_tag)
