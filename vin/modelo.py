ef0693a071f68e7b67d2edf84d1591748cd24b5d
Asmilex <andresmm@outlook.com>
2021-10-27 19:25:30 +0200

    Función para cargar las imágenes (#17)

--- vin/modelo.py
+++ vin/modelo.py
@@ -1,8 +1,9 @@
 from vin.vin_image import VinImage
+import os
+import numpy as np
 
-def cargar_imagenes() -> list(VinImage):
-    """
-    Carga la estructura de imágenes, almacenada de la siguiente forma:
+def cargar_imagenes(directorio: str) -> list[VinImage]:
+    """Carga la estructura de imágenes, almacenada de la siguiente forma:
     ```
     img
     |-> clase1
@@ -13,11 +14,27 @@ def cargar_imagenes() -> list(VinImage):
     ...
     ```
     """
-    pass
+    imagenes = []
 
-def knn(imagen: VinImage, dataset: list(VinImage), k: int) -> str:
+    categorias = os.listdir(directorio)
+
+    print("Comienza la carga de imágenes...")
+    for subcarpeta in os.scandir(directorio):
+        print(f'\t→ Cargando categoría {subcarpeta.name}')
+        for img in os.scandir(subcarpeta):
+            if (img.is_file):
+                imagenes.append(
+                    VinImage(img.path)
+                )
+
+    print("Listo ✓")
+    return imagenes
+
+
+def knn(imagen: VinImage, dataset: list[VinImage], k: int) -> str:
+    """Aproxima `imagen` a los `k` vecinos más próximos utilizando las imágenes presentes en `dataset`
     """
-    Aproxima `imagen` a los `k` vecinos más próximos utilizando las imágenes presentes en `dataset`
-    """
+
+    tag = 'unknown'
 
     pass
