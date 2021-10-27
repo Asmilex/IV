968044f5b2ec1b5950ec5f7f72d50e7945e6f328
Asmilex <andresmm@outlook.com>
2021-10-27 12:38:32 +0200

    `VinImage` usa opencv en vez de pillow (#15)

--- vin/vin_image.py
+++ vin/vin_image.py
@@ -1,16 +1,16 @@
-from PIL import Image
+import cv2 as cv
 from typing import Tuple
 
 class VinImage:
     def __init__(self, file_path: str):
-        self.image = Image.open(file_path)
+        self.image = cv.imread(file_path)
         self.tag = 'unknown'    # Correspondiente a la carpeta en la que se encuentra.
 
     def change_tag(self, nuevo_tag: str):
         self.tag = nuevo_tag
 
     def resolution(self) -> Tuple[int, int]:
-        return self.image.size
+        return (self.image.shape[0], self.image.shape[1])
 
     def downscale(self, resolucion: Tuple[int, int]):
-        self.image = self.image.resize(resolucion)
+        self.image = cv.resize(self.image, resolucion, interpolation=cv.INTER_AREA)
