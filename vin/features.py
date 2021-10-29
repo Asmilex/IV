0538431d34a14c64b7142a5ffac92f6e1eae059e
Asmilex <andresmm@outlook.com>
2021-10-29 13:48:20 +0200

    Reestructura extracción de características (#12, #15)

--- vin/features.py
+++ vin/features.py
@@ -1,23 +1,3 @@
 from vin.vin_image import VinImage
 import cv2 as cv
 import numpy as np
-
-def hu_moments(imagen: VinImage):
-    features = cv.HuMoments(cv.moments(imagen.grayscale()))
-    return features.flatten()
-
-def histogram(imagen: VinImage):
-    size = 32
-    hist = cv.calcHist(
-        images   = [imagen.hsv()],
-        channels = [0, 1, 2],
-        mask     = None,
-        ranges   = [0, 256, 0, 256, 0, 256],
-        histSize = [size, size, size]
-    )
-    cv.normalize(hist, hist)
-    return hist.flatten()
-
-def extract_features(image: VinImage):
-    features = np.hstack([hu_moments(image), histogram(image)])
-    return features
