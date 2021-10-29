abdb4d9118166e2fe1e17220cf1027f4d4c2cafa
Asmilex <andresmm@outlook.com>
2021-10-29 13:48:57 +0200

    knn utiliza distancia propia en vez de la de numpy (#16)

--- vin/modelo.py
+++ vin/modelo.py
@@ -1,8 +1,15 @@
-from vin.features import extract_features
 from vin.vin_image import VinImage
 import os
 import numpy as np
 from typing import List
+
+def distancia(lista1, lista2) -> float:
+    """
+    Calcula la distancia euclídea de dos listas
+    """
+    cuadrados = [(p - q)**2 for p, q in zip(lista1, lista2)]
+    return sum(cuadrados)**.5
+
 
 def cargar_imagenes(directorio: str) -> List[VinImage]:
     """Carga la estructura de imágenes, almacenada de la siguiente forma:
@@ -40,24 +47,22 @@ def knn(imagen: VinImage, dataset: List[VinImage], k: int) -> str:
     tag = 'unknown'
 
     # Preparamos ambas entradas: tanto la imagen a aproximar como el modelo
-    dataset_features = []
-
-    for img in dataset:
-        dataset_features.append(extract_features(img))
-
-    img_features = extract_features(imagen)
+    dataset_features = [img.extract_features() for img in dataset]
+    img_features = imagen.extract_features()
 
 
     # Calculamos las distancias a los elementos del espacio. Estamos buscando las `k` imágenes más cercanas
     distancias = []
 
-    for v in img_features:
+    for v in dataset_features:
         distancias.append(
-            np.linalg.norm(v - img_features)
+            distancia(img_features, v)
         )
+
 
     # Necesitamos ver quién se ha quedado más cerca. Para ello, intentamos ordenar las distancias de menor a mayor, y vemos qué imágenes se han quedado más cerca.
     distancias, dataset_ordenado = zip(*sorted(zip(distancias, dataset)))
+
 
     # Mirar las claves que hay en dataset por orden. Ir haciendo recuento, y quedarnos con la mayoritaria
     diccionario = {}
