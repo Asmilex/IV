b6d3ae374c088398d3b8f90b321e6af91f84bac8
Asmilex <andresmm@outlook.com>
2021-11-04 12:57:51 +0100

    Uso de loggers en la carga de carpetas (#20)

--- vin/file_io.py
+++ vin/file_io.py
@@ -2,6 +2,7 @@ from vin.vin_image import VinImage
 
 from typing import List
 import os
+import logging
 
 def cargar_imagenes(directorio: str) -> List[VinImage]:
     """Carga la estructura de imágenes, almacenada de la siguiente forma:
@@ -17,14 +18,14 @@ def cargar_imagenes(directorio: str) -> List[VinImage]:
     """
     imagenes = []
 
-    print("Comienza la carga de imágenes...")
+    logging.info("Comienza la carga de imágenes...")
     for subcarpeta in os.scandir(directorio):
-        print(f'\t→ Cargando categoría {subcarpeta.name}')
+        logging.info(f'\t→ Cargando categoría {subcarpeta.name}')
         for img in os.scandir(subcarpeta):
             if (img.is_file):
                 imagenes.append(
                     VinImage(img.path, subcarpeta.name)
                 )
 
-    print("Listo ✓")
+    logging.info("Listo ✓")
     return imagenes
