d5f95a7b5b29318e2c43c904965f5d897266319b
Asmilex <andresmm@outlook.com>
2021-12-03 14:19:21 +0100

    Uso de configuración en tests (#31)

--- test/test_workflow.py
+++ test/test_workflow.py
@@ -1,41 +1,58 @@
 import vin.modelo
 import vin.file_io
 from vin.vin_image import VinImage
+
 import pytest
+import toml
+
+config = toml.load('./vin_config.toml')
 
 def test_image_creation():
-    imagen = VinImage('./test/img/test_image.jpg')
+    path = config['test']['img_folder'] + config['test']['img_filename']
+    imagen = VinImage(path)
     assert imagen != None
 
 def test_image_downscale():
     resolucion: tuple[int, int] = (64, 64)
-    imagen = VinImage('./test/img/test_image.jpg')
+    path = config['test']['img_folder'] + config['test']['img_filename']
+
+    imagen = VinImage(path)
     imagen.downscale(resolucion)
+
     assert imagen.resolution() == resolucion
 
 def test_image_tag():
-    imagen = VinImage('./test/img/test_image.jpg')
+    path = config['test']['img_folder'] + config['test']['img_filename']
+    imagen = VinImage(path)
+
     nuevo_tag = 'prueba!'
     imagen.change_tag(nuevo_tag)
+
     assert nuevo_tag == imagen.tag
 
 
 def test_image_vectorization():
-    imagen = VinImage('./test/img/test_image.jpg')
+    path = config['test']['img_folder'] + config['test']['img_filename']
+    imagen = VinImage(path)
+
     features = imagen.extract_features()
+
     assert len(features) != 0, "La lista de caracerísticas está vacía"
 
 def test_carga_imagenes():
-    path = './vin/img'
+    path = config['vin']['img_folder']
     lista = vin.file_io.cargar_imagenes(path)
+
     assert len(lista) != 0, "No se han cargado correctamente las imágenes"
 
 def test_knn():
-    path = './vin/img'
-    imagen = VinImage('./test/img/test_image.jpg')
-    dataset = vin.file_io.cargar_imagenes(path)
+    dataset = vin.file_io.cargar_imagenes(config['vin']['img_folder'])
 
-    k = 1
+    path = config['test']['img_folder'] + config['test']['img_filename']
+    imagen = VinImage(path)
+
+    k = config['test']['k']
+
     tag = vin.modelo.knn(imagen, dataset, k)
 
     assert tag != 'unknown', "No se ha creado correctamente la etiqueta en knn"
