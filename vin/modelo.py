b61073941831dac90e6a53ccdabe4a94a6c4d114
Asmilex <andresmm@outlook.com>
2021-12-10 13:38:49 +0100

    Quito logging antiguo y formateo de archivos (#30)

--- vin/modelo.py
+++ vin/modelo.py
@@ -2,29 +2,24 @@ from vin.vin_image import VinImage
 from vin.utils import distancia
 from typing import List
 
-def knn(imagen: VinImage, dataset: List[VinImage], k: int) -> str:
-    """Aproxima `imagen` a los `k` vecinos más próximos utilizando las imágenes presentes en `dataset`
-    """
 
-    tag = 'unknown'
+def knn(imagen: VinImage, dataset: List[VinImage], k: int) -> str:
+    """Aproxima `imagen` a los `k` vecinos más próximos utilizando las imágenes presentes en `dataset`"""
+
+    tag = "unknown"
 
     # Preparamos ambas entradas: tanto la imagen a aproximar como el modelo
     dataset_features = [img.extract_features() for img in dataset]
     img_features = imagen.extract_features()
 
-
     # Calculamos las distancias a los elementos del espacio. Estamos buscando las `k` imágenes más cercanas
     distancias = []
 
     for v in dataset_features:
-        distancias.append(
-            distancia(img_features, v)
-        )
-
+        distancias.append(distancia(img_features, v))
 
     # Necesitamos ver quién se ha quedado más cerca. Para ello, intentamos ordenar las distancias de menor a mayor, y vemos qué imágenes se han quedado más cerca.
     distancias, dataset_ordenado = zip(*sorted(zip(distancias, dataset)))
-
 
     # Mirar las claves que hay en dataset por orden. Ir haciendo recuento, y quedarnos con la mayoritaria
     recuento = {}
@@ -35,7 +30,6 @@ def knn(imagen: VinImage, dataset: List[VinImage], k: int) -> str:
             recuento[tag] = 1
         else:
             recuento[tag] = recuento[tag] + 1
-
 
     tag = max(recuento, key=recuento.get)
 
